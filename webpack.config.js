2a6bfee596841e58eb5c0760b3fce2cc0786df1f
devmentor.pl <55095257+devmentor-pl@users.noreply.github.com>
2022-11-18 11:23:36 +0100

    update config to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,32 @@ const path = require('path');
 // importuję bibliotękę [path] z [node.js]
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // importuję plugin [html-webpack-plugin]
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// importuję plugin [clean-webpack-plugin]
+const CssWebpackPlugin = require('mini-css-extract-plugin');
+// importuję plugin [mini-css-extract-plugin]
+
 module.exports = function(env = {}) {
 
     const {production: isProd = false} = env;
+    console.log(env);
     // w zmiennej [isProd] przechowuje informację
-    // w jakim trybie uruchomić webpack-a
+    // w jakim trybie uruchomić webpacka
 
     return  {
         entry: './src/app.js',
         // definiuje plik wejściowy
         mode: isProd ? 'production' : 'development',
-        // definiuje tryb pracy webpack-a
+        // definiuje tryb pracy webpacka
         devtool: isProd ? 
-            'none' : 'eval-cheap-module-source-map',
+            false : 'source-map',
         // definiuje identyfikację kodu źródłowego
         output: {
             path: path.resolve(__dirname, 'build'),
             // definiuje ścieżką wyjściową
-            filename: 'app.[hash].js',
+            filename: 'app.[contenthash].js',
             // definiuję nazwę pliku wyjściowego
+            clean: true,
+            // czyszczę katalog build przed nowym buildem
         },
-        target: "web", 
-        // niezbędne do uruchomienia automatycznego odświeżania z webpack-dev-server@3
-        // https://github.com/webpack/webpack-dev-server/issues/2758
         module: {
             rules: [
                 {
@@ -40,34 +41,73 @@ module.exports = function(env = {}) {
                     // ma być wykorzystany
                 },
                 {
+                    test: /\.css$/,
+                    // tylko pliki z rozszerzeniem .css
+                    exclude: /node_modules/,
+                    // wykluczam pliki zwierającą 
+                    // wpisany ciąg znaków w ścieżce
+                    use: [
+                        isProd ? CssWebpackPlugin.loader : 'style-loader', 
+                        //'css-loader'
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: isProd ? false : true,
+                                // ustawiam identyfikację kodu źródłowego
+                            },
+                        },
+                    ],
+                    // określam kolejność wykorzystanych 
+                    // loader-ów tj. od lewej do prawej (lub o dołu do góry)
+                },
+                {
+                    test: /\.scss$/,
+                    // tylko pliki z rozszerzeniem .scss
+                    use: [
+                        isProd ? CssWebpackPlugin.loader : 'style-loader', 
+                        //'css-loader', 
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: isProd ? false : true,
+                                // ustawiam identyfikację kodu źródłowego
+                            },
+                        },
+                        // 'sass-loader'
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: isProd ? false : true,
+                                // ustawiam identyfikację kodu źródłowego
+                                sassOptions: {
+                                    outputStyle: isProd ? 'compressed' : 'expanded',
+                                    // ustawiam sposób zapisu kodu CSS
+                                },
+                            },
+                            
+                        }
+                    ],
+                    // określam kolejność wykorzystanych 
+                    // loader-ów tj. od lewej do prawej
+
+                },
+                {
                     test: /\.(png|svg|jpg|gif)$/,
                     // dodaję rozszerzenia obrazów
-                    use: {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[hash].[ext]',
-                            // ustawiam nazwę pliku
-                            outputPath: 'images',
-                            // ustawiam nazwę katalogu, do którego
-                            // będą kopiowane obrazy
-                        }
-                    }
-                    // tym razem tylko jeden loader
+                    type: 'asset/resource',
+                    generator: {
+                        filename: 'images/[name][contenthash][ext]',
+                        // ustawiam nazwę katalogu i pliku
+                    },
                 },
                 {
                     test: /\.(ttf|otf|woff|woff2)$/,
-                    // dodaję rozszerzenia obrazów
-                    use: {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[hash].[ext]',
-                            // ustawiam nazwę pliku
-                            outputPath: 'fonts',
-                            // ustawiam nazwę katalogu, do którego
-                            // będą kopiowane font-y
-                        }
-                    }
-                    // tym razem tylko jeden loader
+                    // dodaję rozszerzenia fontów
+                    type: 'asset/resource',
+                    generator: {
+                        filename: 'fonts/[name][contenthash][ext]',
+                        // ustawiam nazwę katalogu i pliku
+                    },
                 },
             ]
         },
@@ -78,9 +118,11 @@ module.exports = function(env = {}) {
                 filename: 'index.html'
                 // określan nazwę dla pliku
             }),
-            new CleanWebpackPlugin(),
-            // uruchamiam czyszczenie katalogu [build]
+            new CssWebpackPlugin({
+                filename: '[name].[contenthash].css',
+                // określam nazwę pliku css
+            })
         ]
     }
 }
-// eksportuję ustawienia dla webpack-a
+// eksportuję ustawienia dla webpacka
