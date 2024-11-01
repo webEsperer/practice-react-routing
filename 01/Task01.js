c9283d86c16fb9ba4e69241e15bf459f43d55076
Piotr Matejek <webesperer@gmail.com>
2024-11-01 22:41:48 +0100

    solved task01

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Route, Link } from "react-router-dom";
+import Home from "./../src/components/Home";
+import Contact from "./../src/components/Contact";
 
 const Task01 = () => {
-    return (
-        <h1>Task01</h1>
-    );
-}
+  return (
+    <>
+      <h1>Task01</h1>
+      <ul>
+        <li>
+          <Link to="/task01/Home">Home</Link>
+        </li>
+        <li>
+          <Link to="/task01/Contact">Contact</Link>
+        </li>
+      </ul>
+
+      <Route path="/task01/Home">
+        <Home />
+      </Route>
+      <Route path="/task01/Contact">
+        <Contact />
+      </Route>
+    </>
+  );
+};
 
 export default Task01;
 
