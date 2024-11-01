7710183b9182cc54e249a5a147f95f51c65d4c50
Piotr Matejek <webesperer@gmail.com>
2024-11-01 23:17:48 +0100

    solved task03

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Route, Link, useParams } from "react-router-dom";
+import products from "./../src/products.json";
+
+const Product = () => {
+  const { id } = useParams();
+  const [product] = products.filter((item) => item.id === Number(id));
+  const { name, description, category, price } = product;
+
+  return (
+    <article>
+      <h2>{name}</h2>
+      <h3>{description}</h3>
+      <h3>{category}</h3>
+      <h3>{price}</h3>
+    </article>
+  );
+};
 
 const Task02 = () => {
-    return (
-        <h1>Task02</h1>
-    );
-}
+  return (
+    <>
+      <h1>Task02</h1>
+      <Route path="/task02/product-:id">
+        <Product />
+      </Route>
+    </>
+  );
+};
 
 export default Task02;
 
